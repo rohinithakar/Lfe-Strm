4f9d42b6de070765d51f15fbf9009decbcf3b95b
swetapatel <swetapatel@gmail.com>
2013-03-31 03:32:37 -0700

    chnaged client

--- poke-py/poke/client/PokeClient.py
+++ poke-py/poke/client/PokeClient.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python
 
-import socket
+import socket, select
 from pygen import comm_pb2
 import time
 import pokeMessage
 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 5570
-BUFFER_SIZE = 1024*1024*1 # 1 MB buffer size
+TCP_IP = 'localhost'
+TCP_PORT = 6570
+BUFFER_SIZE = 65535 # 1 MB buffer size
 MESSAGE = "Hello, World!"
 
 def sendRequest( message ):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((TCP_IP, TCP_PORT))
-    s.send(message)
-    data = s.recv(BUFFER_SIZE)
+    s.sendall(message)
+    res = ""
+    data = s.recv(1024)
+    while len(data):
+        res = res + data
+        data = s.recv(1024)
     s.close()
-    return data
+    return res
 
 def register( emailid, fname, lname, password ):
     message = pokeMessage.register(fname, lname, emailid, password)
