b1c4470c9f13299acd0806126f8e30b12920a4d8
rohinithakar <rohini.thakar85@gmail.com>
2013-03-31 22:11:22 -0700

    Working python client for login and register Added code for imageUpload and download

--- poke-py/src/poke/client/PokeClient.py
+++ poke-py/src/poke/client/PokeClient.py
@@ -1,32 +1,84 @@
 #!/usr/bin/env python
 
-import socket, select, time, struct
-import pokeMessage
+import socket, struct, io
+import PokeMessage
 from pygen import comm_pb2
 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 6570
-MESSAGE = "Hello, World!"
+IP = '127.0.0.1'
+TCP_PORT = 6571
 
-def sendRequest( message ):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((TCP_IP, TCP_PORT))
-    size = struct.pack(">l", len(message))
-    print "Bytes Sent: " + str(s.send(size + message ))
-    # print "Bytes Sent2: " + str(s.send(message))
-    dataSize = s.recv(4)
-    size = struct.unpack(">l",dataSize)
-    data = s.recv(size[0])
-    resp = comm_pb2.Response()
-    resp.ParseFromString(data)
-
-    s.close()
-    return resp
-
-def register( emailid, fname, lname, password ):
-    message = pokeMessage.register(fname, lname, emailid, password)
-    resp = sendRequest(message)
-    print "Reply received"
-    return resp.header.reply_code
+class PokeClient:
+    __hostname = None
+    __port = None
+    __pm = None
     
-print register("weupqewrqwerqwrqowr@owerweupru.com","a", "b", "1234")
+    def __init__(self, hostname, port):
+        self.__hostname = hostname
+        self.__port = port
+        self.__pm = PokeMessage.PokeMessage('PythonClient')
+
+    def __sendRequest( self, message ):
+        resp = None
+        try:
+            socketObj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            socketObj.connect((self.__hostname, self.__port))
+            size = struct.pack(">l", len(message))
+            socketObj.send(size + message )
+            dataSize = socketObj.recv(4)
+            size = struct.unpack(">l",dataSize)
+            data = socketObj.recv(size[0])
+            print "Received Reply"
+            resp = comm_pb2.Response()
+            
+            resp.ParseFromString(data)
+        except IOError as e:
+            print "I/O error({0}): {1}".format(e.errno, e.strerror)
+        except struct.error:
+            print "Error in reading response"
+            resp = None
+        finally:
+            socketObj.close()
+        return resp
+
+    def register( self, emailid, fname, lname, password ):
+        message = self.__pm.register(fname, lname, emailid, password)
+        resp = self.__sendRequest(message)
+        if resp == None:
+            return None
+        return resp.header.reply_code == 1
+
+    def login(self, emailid, password):
+        message = self.__pm.login(emailid, password)
+        resp = self.__sendRequest(message)
+        if resp == None:
+            return None
+        return resp.header.reply_code == 1
+    
+    def getImages(self, emailid):
+        message = self.__pm.getImages(emailid)
+        resp = self.__sendRequest(message)
+        if resp == None:
+            return None
+        return resp
+    
+    def __readImageFile(self, filePath):
+        with io.open(filePath, 'rb') as fileObj:
+            data = fileObj.readAll()
+        return data
+    
+    def uploadImage(self, emailid, imagePath, latitude, longitude, title ):
+        img = comm_pb2.Image()
+        img.actualImage = self.__readImageFile(imagePath)
+        img.latitude = latitude
+        img.longitude = longitude
+        img.title = title
+        img.ownerEmail = emailid
+        message = self.__pm.uploadImage(emailid, img)
+        resp = self.__sendRequest(message)
+        if resp == None:
+            return None
+        return resp
+    
+pc = PokeClient(IP,TCP_PORT)
+print pc.register("abc@abc.com","a", "b", "1234")
+print pc.login("abc@abc.com",'123')
---
+++ poke-py/src/poke/client/PokeMessage.py
@@ -0,0 +1,75 @@
+
+from pygen import comm_pb2
+import time
+
+class PokeMessage:
+    __clientName = ''
+
+    def __init__(self, clientNameParam='PythonClient'):
+        self.__clientName = clientNameParam 
+
+    def register(self, fname,lname,email,password):
+        r = comm_pb2.Request()
+        p = comm_pb2.Payload()
+        p.__setattr__("emailid",email)
+    
+        reg = comm_pb2.Register()
+        reg.__setattr__("fname",fname)
+        reg.__setattr__("lname",lname)
+        reg.__setattr__("password",password)
+        p.reg.MergeFrom(reg)    
+        r.body.MergeFrom(p)
+    
+        #Header with routing Info
+        h=comm_pb2.Header()    
+        h.__setattr__("originator",self.__clientName)
+        h.__setattr__("time",int(time.time()))
+        h.__setattr__("routing_id",int(h.__getattribute__("REGISTER")))  #comm_pb2.Header.DESCRIPTOR.__getattribute__("REGISTER")
+        r.header.MergeFrom(h)   
+        return r.SerializeToString()
+
+    def getImages(self, email):
+        r = comm_pb2.Request()
+        p=comm_pb2.Payload()
+        p.__setattr__("emailid",email)
+        r.body.MergeFrom(p)
+               
+        h=comm_pb2.Header()
+        h.__setattr__("originator",self.__clientName)
+        h.__setattr__("time",int(time.time()))
+        h.__setattr__("routing_id",int(h.__getattribute__("REGISTER")))  #comm_pb2.Header.DESCRIPTOR.__getattribute__("REGISTER")
+        
+        r.header.MergeFrom(h)        
+        return r.SerializeToString()
+
+    def uploadImage(self,email,img):
+        r = comm_pb2.Request()
+        p=comm_pb2.Payload()
+        p.__setattr__("emailid",email)
+        p.imageup.MergeFrom(img)
+        
+        r.body.MergeFrom(p)
+        
+        h=comm_pb2.Header()
+        h.__setattr__("originator",self.__clientName)
+        h.__setattr__("time",int(time.time()))
+        h.__setattr__("routing_id",int(h.__getattribute__("REGISTER")))  #comm_pb2.Header.DESCRIPTOR.__getattribute__("REGISTER")
+        
+        r.header.MergeFrom(h)
+        
+        return r.SerializeToString()
+
+    def login(self, emailid, password):
+        r = comm_pb2.Request()
+        p = comm_pb2.Payload()
+        p.emailid = emailid
+        login = comm_pb2.Login()
+        login.password = password
+        p.login.MergeFrom(login)
+        r.body.MergeFrom(p)
+        h=comm_pb2.Header()
+        h.__setattr__("originator",self.__clientName)
+        h.__setattr__("time",int(time.time()))
+        h.__setattr__("routing_id",int(h.__getattribute__("LOGIN")))
+        r.header.MergeFrom(h)
+        return r.SerializeToString()
